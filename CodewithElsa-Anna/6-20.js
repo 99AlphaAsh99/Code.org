5cc8f553a160a7174557711b95792af1364918a1
Ella975 <Aisling@behuman.org>
2017-05-29 15:20:58 +0100

    Create 6-20.js

---
+++ CodewithElsa-Anna/6-20.js
@@ -0,0 +1,114 @@
+for (var count2 = 0; count2 < 10; count2++) {
+  for (var count = 0; count < 4; count++) {
+    moveForward(100);
+    turnRight(90);
+  }
+  turnRight(36);
+}
+
+
+
+for (var count = 0; count < 4; count++) {
+  moveForward(100);
+  moveBackward(100);
+  turnRight(90);
+}
+ReplayContinue
+
+
+
+
+for (var count = 0; count < 10; count++) {
+  moveForward(100);
+  moveBackward(100);
+  turnRight(36);
+}
+ReplayContinue
+
+
+
+for (var count = 0; count < 90; count++) {
+  penColour(colour_random());
+  moveForward(100);
+  moveBackward(100);
+  turnRight(4);
+}
+
+
+
+
+for (var count = 0; count < 90; count++) {
+  penColour(colour_random());
+  moveForward(100);
+  moveBackward(100);
+  turnRight(4);
+}
+
+
+
+for (var count2 = 0; count2 < 4; count2++) {
+  for (var count = 0; count < 2; count++) {
+    penColour('#aea4ff');
+    moveForward(100);
+    turnRight(60);
+    moveForward(100);
+    turnRight(120);
+  }
+  turnRight(90);
+}
+
+
+
+penColour('#aea4ff');
+for (var count2 = 0; count2 < 10; count2++) {
+  for (var count = 0; count < 2; count++) {
+    moveForward(100);
+    turnRight(60);
+    moveForward(100);
+    turnRight(120);
+  }
+  turnRight(36);
+}
+
+
+
+for (var count = 0; count < 360; count++) {
+  moveForward(1);
+  turnRight(1);
+}
+
+
+
+for (var count2 = 0; count2 < 10; count2++) {
+  // create_a_circle
+  for (var count = 0; count < 36; count++) {
+    moveForward(10);
+    turnRight(10);
+  }
+  jumpForward(25);
+}
+
+
+
+for (var count2 = 0; count2 < 20; count2++) {
+  // create_a_circle
+  for (var count = 0; count < 36; count++) {
+    moveForward(10);
+    turnRight(10);
+  }
+  jumpForward(50);
+  turnRight(18);
+}
+
+
+
+// create_a_circle
+for (var count = 0; count < 36; count++) {
+  moveForward((5));
+  turnRight(10);
+}
+// create_a_circle
+for (var count2 = 0; count2 < 36; count2++) {
+  moveForward((10));
+  turnRight(10);
+}
